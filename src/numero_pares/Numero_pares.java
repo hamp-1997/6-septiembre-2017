e84731c0206c93399c2d91de6e711b11c0697c48
HEBER MORALES <HEBER MORALES@192.168.28.12>
2017-09-06 10:12:03 -0700

    practicas 06/septiembre/2017

---
+++ src/numero_pares/Numero_pares.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package numero_pares;
+
+/**
+ *
+ * @author HEBER MORALES
+ */
+public class Numero_pares {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
